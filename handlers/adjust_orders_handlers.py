67784f587ed87362136dc41a176d38dc38a0496c
funnymonke12 <funnimonke12@gmail.com>
2023-07-11 20:31:13 +0500

    add func that change delivery_status

--- handlers/adjust_orders_handlers.py
+++ handlers/adjust_orders_handlers.py
@@ -1,7 +1,12 @@
 from aiogram import types, Dispatcher
-from keyboards import main_kb, change_status_kb
-from ozon import get_data
+from keyboards import main_kb, change_status_kb, inline_change_kb
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from ozon import get_data, write_delivered, write_delivering, write_last_mile, write_sended_by_seller
 from config import client_id, api_key
+
+class Form(StatesGroup):
+    status = State()
 
 
 
@@ -14,17 +19,20 @@ async def send_welcome(message: types.Message):
 async def watch_orders(message: types.Message):
     lst_data = get_data(client_id, api_key)
     articul = lst_data[0]
-    date = lst_data[1]
-    quantity = lst_data[2]
-    price = lst_data[3]
-    address = lst_data[4]
-    address_client = lst_data[5]
-    phone_client = lst_data[6]
-    comment_client = lst_data[7]
-    comment_taxi = lst_data[8]
+    date_begin = lst_data[1]
+    date_end = lst_data[2]
+    quantity = lst_data[3]
+    price = lst_data[4]
+    address = lst_data[5]
+    address_client = lst_data[6]
+    phone_client = lst_data[7]
+    comment_client = lst_data[8]
+    comment_taxi = lst_data[9]
+    delivery_status = lst_data[10]
+    posting_number = lst_data[11]
     await message.answer(
 f"""Артикул: {articul}
-Дата отправления-доставки: {date}
+Дата отправления-доставки: {date_begin}-{date_end}
 Количество: {quantity}
 Цена: {price}
 Адресс: {address}
@@ -32,14 +40,33 @@ f"""Артикул: {articul}
 Телефон клиента: {phone_client}
 Комментарий клиента {comment_client}
 Комментарий для водителя: {comment_taxi}
-статус статус: """)
+статус статус: {delivery_status}
+Номер поставки: {posting_number}""", reply_markup=inline_change_kb)
     
 # @dp.callback_query_handler(func=lambda c: c.data == 'change')
-# async def process_callback_button1(callback_query: types.CallbackQuery):
-#     await callback_query.message.answer('Выберите статус доставки', reply_markup=change_status_kb)    
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await callback_query.message.answer('Выберите статус доставки', reply_markup=change_status_kb)
+    await Form.status.set()
+
+async def process_status(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['status'] = message.text
+        status = data['status']
+
+    await message.answer(f" {status}")
+    if status == 'Доставляется':
+        write_delivering(client_id, api_key)
+    elif status == 'Последняя миля':
+        write_last_mile(client_id, api_key)
+    elif status == 'Доставлено':
+        write_delivered(client_id, api_key)
+    elif status == 'Отправлено продавцом':
+        write_sended_by_seller(client_id, api_key)
+    await state.finish()
 
 
 def register_handlers_adjust(dp: Dispatcher):
     dp.register_message_handler(send_welcome, commands=["start", "help"])
     dp.register_message_handler(watch_orders, commands=["watch_orders"])
-    # dp.register_callback_query_handler(process_callback_button1, function=lambda c: c.data == 'change')
+    dp.register_callback_query_handler(process_callback_button1, lambda c: c.data == 'change')
+    dp.register_message_handler(process_status, state=Form.status)
--- ozon.py
+++ ozon.py
@@ -2,6 +2,7 @@ import requests
 from config import client_id, api_key
 
 def get_data(id, key):
+    global posting_number
     # url = 'https://api-seller.ozon.ru/v3/posting/fbs/unfulfilled/list'
     # params = {"with": {
     #             "analytics_data": True,
@@ -14,6 +15,7 @@ def get_data(id, key):
     # }
     # response = requests.post(url, params).text
     # posts = response["result"]["postings"][0]
+    # delivery_status = posts["status"]
     # product_data = posts["products"][0]
     # product_id = product_data["offer_id"]
     # delivery_date_begin = posts["analytics_data"]['delivery_date_begin']
@@ -27,6 +29,7 @@ def get_data(id, key):
     # comment = customer["address"]["comment"]
     # latitude = customer["address"]["latitude"]
     # longitude = customer["address"]["longitude"]
+    # posting_number = posts["posting_number"]
     product_data = 'Какой-то продук'
     product_id = 'Айди'
     delivery_date_begin = 'Начало даты'
@@ -38,8 +41,57 @@ def get_data(id, key):
     second_address = 'Адресс клиента'
     client_phone = 'Телефон клиента'
     comment = 'Комментарий клиента'
+    dr_comment = 'Комментарий для водителя'
+    delivery_status = "awaiting_packaging"
+    posting_number = 'Номер поставки'
     # latitude = customer["address"]["latitude"]
     # longitude = customer["address"]["longitude"]'comment_dr'
-    return product_data, product_id, delivery_date_begin, delivery_date_end, quantity, price, first_address, second_address, client_phone, comment
+    return product_id, delivery_date_begin, delivery_date_end, quantity, price, first_address, second_address, client_phone, comment, dr_comment, delivery_status, posting_number
 
+def get_posting_number(id, api):
+    # url = 'https://api-seller.ozon.ru/v3/posting/fbs/unfulfilled/list'
+    # params = {"with": {
+    #             "analytics_data": True,
+    #             "barcodes": True,
+    #             "financial_data": True,
+    #             "translit": True
+    #             },
+    #     'Client-Id': client_id,
+    #     'Api-Key': api_key
+    # }
+    # response = requests.post(url, params).text
+    # posts = response["result"]["postings"][0]
+    # posting_number = posts["posting_number"]
+    # return posting_number
+    pass
+def write_delivering(id, api):
+    # url = 'https://api-seller.ozon.ru/v2/fbs/posting/delivering'
+    # query = {'Client-Id': id,
+    #          'Api-Key': api,
+    #          "posting_number": [get_posting_number(client_id, api_key)]}
+    # requests.post(url, params=query)
+    pass
+def write_last_mile(id, api):
+    # url = 'https://api-seller.ozon.ru/v2/fbs/posting/last-mile'
+    # query = {'Client-Id': id,
+    #          'Api-Key': api,
+    #          "posting_number": [get_posting_number(client_id, api_key)]}
+    # requests.post(url, params=query)
+    pass
+
+def write_delivered(id, api):
+    # url = 'https://api-seller.ozon.ru/v2/fbs/posting/delivered'
+    # query = {'Client-Id': id,
+    #          'Api-Key': api,
+    #          "posting_number": [get_posting_number(client_id, api_key)]}
+    # requests.post(url, params=query)
+    pass
+
+def write_sended_by_seller(id, api):
+    # url = 'https://api-seller.ozon.ru/v2/fbs/posting/sent-by-seller'
+    # query = {'Client-Id': id,
+    #          'Api-Key': api,
+    #          "posting_number": [get_posting_number(client_id, api_key)]}
+    # requests.post(url, params=query)
+    pass
 
